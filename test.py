3bccf719ac5bd95297d9e46f8a07d19fd2217d1d
hwb0218 <52212226+hwb0218@users.noreply.github.com>
2019-08-14 22:23:14 +0900

    데이터 DB저장

---
+++ test.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, jsonify, request
+import requests
+from bs4 import BeautifulSoup
+
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/list')
+def home_list():
+    return render_template('order_list.html')
+
+@app.route('/save', methods=['POST'])
+def saving():
+    firstName_receive = request.form['firstName_give']
+    lastName_receive = request.form['lastName_give']
+    to_receive = request.form['to_give']
+    from_receive = request.form['from_give']
+    email_receive = request.form['email_give']
+    departure_receive = request.form['departure_give']
+    destination_receive = request.form['destination_give']
+    passenger_receive = request.form['passenger_give']
+
+    ticket = {
+
+        'firstName' : firstName_receive,
+        'lastName' : lastName_receive,
+        'to' : to_receive,
+        'from' : from_receive,
+        'email' : email_receive,
+        'departure' : departure_receive,
+        'destination' : destination_receive,
+        'passenger' : passenger_receive
+
+    }
+    db.tickets.insert_one(ticket)
+    return jsonify({'result' : 'success'})
+
+@app.route('/save', methods=['GET'])
+def get_view():
+
+    save = db.tickets.find({}, {'id': 0})
+    return jsonify({'result': 'success', 'tickets': list(save)})
+
+if __name__ == '__main__':
+   app.run('0.0.0.0',port=5002,debug=True)
